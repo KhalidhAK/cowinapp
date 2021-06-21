9762ec7008f40b00ec9f0acd1958bbc349f3bcd5
Khalidh Ahamed A <Khalidh.Ahamed@mrcooper.com>
2021-06-21 20:00:50 +0530

    first push

---
+++ int/model/Discussion.py
@@ -0,0 +1,4 @@
+class Discussion:
+    def __init__(self=None, question=None, answer=None):
+        self.question = question
+        self.answer = answer
---
+++ int/model/User.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self=None, user_id=None, user_name=None, user_password=None, user_purchased_courses=None, wallet_balance=None):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.user_password = user_password
+        self.user_purchased_courses = user_purchased_courses
+        self.wallet_balance = wallet_balance
